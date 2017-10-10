464d38de3c25d707717209f5c38eb36a3a3cea80
dukefu <programduke@gmail.com>
2017-10-10 16:20:59 -0700

    Added most of comments

--- src/main/java/calculator/ast/ExpressionManipulators.java
+++ src/main/java/calculator/ast/ExpressionManipulators.java
@@ -7,38 +7,32 @@ import datastructures.interfaces.IDictionary;
 import datastructures.interfaces.IList;
 import misc.exceptions.NotYetImplementedException;
 
-/**
- * All of the static methods in this class are given the exact same parameters
- * for consistency. You can often ignore some of these parameters when
- * implementing your methods.
- *
- * Some of these methods should be recursive. You may want to consider using
- * public-private pairs in some cases.
- */
+//This is a part of a Symbolic algebra calculator. It does not immediately
+//evaluate the expressions the user type in buy instead lets user manipulate
+//them symbolically. It can simplify an expression, convert an expression to
+//a double and plot an expression. 
 public class ExpressionManipulators {
-	/**
-	 * Takes the given AstNode node and attempts to convert it into a double.
-	 *
-	 * Returns a number AstNode containing the computed double.
-	 *
-	 * @throws EvaluationError
-	 *             if any of the expressions contains an undefined variable.
-	 * @throws EvaluationError
-	 *             if any of the expressions uses an unknown operation.
-	 */
+	//Pass an Environment object and AstNode object as parameters.
+	//return a node which attempts to evaluate the given 
+	//AstNode into a single double node. Return an AstNode containing 
+	//the computed double
+	//Throws EvaluationError if any of the expressions contains an 
+	//undefined variable
+	//Throw EvaluationError if any of the expressions uses an unknown oper
 	public static AstNode toDouble(Environment env, AstNode node) {
-		// To help you get started, we've implemented this method for you.
-		// You should fill in the TODOs in the 'toDoubleHelper' method.
 		return new AstNode(toDoubleHelper(env.getVariables(), node));
 	}
 
+	//Pass an IDictionary<> Object and a AstNode object as parameters.
+	//return a double which attempts to evaluate the given AstNode.
+	//Throws EvaluationError if any of the expressions contains an 
+	//undefined variable
+	//Throw EvaluationError if any of the expressions uses an unknown operation
 	private static double toDoubleHelper(IDictionary<String, AstNode> variables, AstNode node) {
-		// There are three types of nodes, so we have three cases.
 		if (node.isNumber()) {
 			return node.getNumericValue();
 		} else if (node.isVariable()) {
 			if (!variables.containsKey(node.getName())) {
-				// If the expression contains an undefined variable, we give up.
 				throw new EvaluationError("Undefined variable: " + node.getName());
 			}
 			return variables.get(node.getName()).getNumericValue();
@@ -87,13 +81,7 @@ public class ExpressionManipulators {
 		}
 	}
 
-	// Try writing this one on your own!
-	// Hint 1: Your code will likely be structured roughly similarly
-	// to your "toDouble" method
-	// Hint 2: When you're implementing constant folding, you may want
-	// to call your "toDouble" method in some way
-
-	// TODO: Remove this comment when we are done testing
+	// TODO: comment
 	public static AstNode simplify(Environment env, AstNode node) {
 		IDictionary<String, AstNode> vars = env.getVariables();
 		if (node.isOperation()) {
@@ -109,6 +97,7 @@ public class ExpressionManipulators {
 		}
 	}
 
+	//TODO: comment
 	public static AstNode simplifyHelper(IDictionary<String, AstNode> vars, AstNode node) {
 		if (node.isOperation()) {
 			if (isExpression(vars, node)) {
@@ -129,40 +118,22 @@ public class ExpressionManipulators {
 		return node;
 	}
 
-	/**
-	 * Expected signature of plot:
-	 *
-	 * >>> plot(exprToPlot, var, varMin, varMax, step)
-	 *
-	 * Example 1:
-	 *
-	 * >>> plot(3 * x, x, 2, 5, 0.5)
-	 *
-	 * This command will plot the equation "3 * x", varying "x" from 2 to 5 in 0.5
-	 * increments. In this case, this means you'll be plotting the following points:
-	 *
-	 * [(2, 6), (2.5, 7.5), (3, 9), (3.5, 10.5), (4, 12), (4.5, 13.5), (5, 15)]
-	 *
-	 * ---
-	 *
-	 * Another example: now, we're plotting the quadratic equation "a^2 + 4a + 4"
-	 * from -10 to 10 in 0.01 increments. In this case, "a" is our "x" variable.
-	 *
-	 * >>> c := 4 4 >>> step := 0.01 0.01 >>> plot(a^2 + c*a + a, a, -10, 10, step)
-	 *
-	 * ---
-	 *
-	 * @throws EvaluationError
-	 *             if any of the expressions contains an undefined variable.
-	 * @throws EvaluationError
-	 *             if varMin > varMax
-	 * @throws EvaluationError
-	 *             if 'var' was already defined
-	 * @throws EvaluationError
-	 *             if 'step' is zero or negative
-	 */
+	// Pass an Environment Object and an AstNode as parameters. The AstNode 
+	// contains data we need to plot an expression. The data contains
+	// expression which is used to plot the image, variable name, 
+	// minimum value of the variable, maximum value of the variable and 
+	// number of increment each time.
+	// It will analysis the node and plot the image using given data, and it 
+	// returns a node which contains simplified version of the
+	// expression(if possible)
+	// throws EvaluationError if any of the expressions contains an 
+	// 		undefined variable.
+	// throws EvaluationError is minimum value of the variable is
+	// 		larger than the maximum value of the variable
+	// throws Evaluation Error if the variable is already defined
+	// throws Evaluation Error if the number of increment is 
+	//		zero or negative
 	public static AstNode plot(Environment env, AstNode node) {
-		
 		AstNode expression = simplify(env, node.getChildren().get(0));
 		double lowerBound = node.getChildren().get(2).getNumericValue();
 		double upperBound = node.getChildren().get(3).getNumericValue();
@@ -191,15 +162,22 @@ public class ExpressionManipulators {
 			env.getVariables().put(varName, new AstNode(xValues.get(i)));
 			yValues.add(toDoubleHelper(env.getVariables(), expression));
 		}
-		env.getVariables().remove(varName); // remove the value added during the loop
+		// remove the temporary value added during the loop
+		env.getVariables().remove(varName); 
 		env.getImageDrawer().drawScatterPlot("plot", varName, "output", xValues, yValues);
 		return expression;
 	}
 
+	// Pass an Environment Object, an AstNode and an integer as parameters. 
+	// Use integer to determine the index of the object we want to get. 
+	// Return a double based on the index of the node's children
 	private static double getNumericValue(Environment env, AstNode node, int index) {
 		return toDouble(env, node.getChildren().get(index)).getNumericValue();
 	}
 
+	// Pass a IList<> object and an integer as parameters. 
+	// If the size of IList<> is not equal to the given integer
+	// Throws an EvaluationError
 	private static void checkNumberOfOperands(IList<AstNode> children, int count) {
 		if (children.size() != count) {
 			throw new EvaluationError(
@@ -207,6 +185,8 @@ public class ExpressionManipulators {
 		}
 	}
 
+	// Pass an IDictionary<> object and an AstNode as parameters.
+	// Return true if the given node is an expression. Otherwise false
 	private static boolean isExpression(IDictionary<String, AstNode> variables, AstNode node) {
 		if (node.isOperation()) {
 			String operationName = node.getName();
@@ -230,6 +210,7 @@ public class ExpressionManipulators {
 			return false;
 		}
 	}
+	
 	/*
 	 * Returns true if a variable evaluates to a constant
 	 * Returns false if the variable evaluates to an expression or is not a variable
@@ -246,7 +227,9 @@ public class ExpressionManipulators {
 		return false;
 	}
 	
-
+	//Pass an Environment object, an AstNode and a String as parameters. 
+	//return false if the given node is not a defined variable and its 
+	//name does not match the given String. Otherwise return ture.
 	private static boolean isDefinedVariable(Environment env, AstNode node, String varName) {
 		if (node.isOperation()) {
 			boolean result = true;
@@ -256,7 +239,6 @@ public class ExpressionManipulators {
 					return result;
 				}
 			}
-			
 		}else if (node.isVariable() && !node.getName().equals(varName)) {
 			if(!isConstant(env.getVariables(), node)) {
 				return env.getVariables().containsKey(node.getName());
